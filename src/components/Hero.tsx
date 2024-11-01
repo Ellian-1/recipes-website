28e32e3e9d99b1609a11f338cb2638274b701d9d
Ellian <ellianalba1@gmail.com>
2024-11-01 15:10:24 -0400

    Create the recipes dynamic page and the NewPage component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { useRouter } from "next/navigation";
 
 type Props = {
   recipes: {
+    id: number;
     name: string;
     steps: string;
     image: string;
   };
 };
 
-const Hero = (recipes: Props) => {
+const Hero = ({ recipes }: Props) => {
+  const router = useRouter();
   return (
-    <div>
+    <div
+      onClick={() => {
+        router.push(`/recipes/edit/${recipes.id}`);
+      }}
+    >
       <img
         className="h-auto max-w-full rounded-lg"
         src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
