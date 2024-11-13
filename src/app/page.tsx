9fe3a66d4bb866e0cdef344edce2593592de4b89
Ellian <ellianalba1@gmail.com>
2024-11-13 14:09:42 -0400

    Add the h-screen to fill the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,10 @@ const loadRecipes = async () => {
 export default async function Home() {
   const recipes = await loadRecipes();
   return (
-    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 h-screen">
       <div className="container max-w-7xl w-full">
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
           {recipes.map((recipe) => (
-            // <TaskCard task={task} key={task.id} />
             <Hero recipes={recipe} key={recipe.id} />
           ))}
         </div>
